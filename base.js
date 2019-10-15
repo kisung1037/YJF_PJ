c80c9161d99672b90085e02ffd8eca42169a199a
kisung1037 <kisung1037@gmail.com>
2019-10-15 12:44:01 +0900

    init commit

---
+++ base.js
@@ -0,0 +1,59 @@
+// var MSUB = document.querySelectorAll(".js-sub"),
+//     MDSUB = document.querySelectorAll(".sub")
+
+// var sub0 = MSUB[0];
+// var sub0_1 = MDSUB[0];
+// var sub1 = MSUB[0];
+// var sub1_1 = MDSUB[0];
+// var sub2 = MSUB[1];
+// var sub2_1 = MDSUB[1];
+// var SUB1_CL = sub1_1.classList;
+// const SUB2_CL = sub2_1.classList;
+
+// function loadMenu() {
+//     sub1.addEventListener("mouseover", subMenu)
+//     sub1_1.addEventListener("mouseleave" , subOut)
+//     sub2.addEventListener("mouseover", subMenu2)
+//     sub2_1.addEventListener("mouseleave" , subOut)
+// }
+
+// function mouseOver() {
+//     while(true){
+//         if(subArr[i] == false){
+//             SUB1_CL.remove("sub");
+//             SUB1_CL.add("subOn");
+//         }else if (subArr[i] == true){
+//             SUB1_CL.remove("subOn");
+//             SUB1_CL.add("sub");
+//         }
+//     }
+// }
+// function subOut() {
+//     SUB1_CL.remove("subOn")
+//     SUB1_CL.add("sub")
+//     // SUB2_CL.remove("subOn")
+//     // SUB2_CL.add("sub")
+// }
+// function subMenu() {
+//         if(SUB1_CL.value !== "subOn"){
+//             SUB1_CL.remove("sub")
+//             SUB1_CL.add("subOn")
+//         }else{
+//             SUB1_CL.remove("subOn")
+//             SUB1_CL.add("sub")
+//         } 
+// }
+// // function subMenu2() {
+//     if(SUB2_CL.value !== "subOn"){
+//         SUB2_CL.remove("sub")
+//         SUB2_CL.add("subOn")
+//     }else{
+//         SUB2_CL.remove("subOn")
+//         SUB2_CL.add("sub")
+//     }
+// }
+
+function init() {
+}
+
+init();
